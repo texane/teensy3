2816ca29beb2272c660be73222edd8dea77ea23e
texane <texane@gmail.com>
2014-04-21 09:18:54 -0500

    [ add, mv ] add pit.c . disable IntervalTimer to use pitX_isr.

---
+++ src/common/pit.c
@@ -0,0 +1,71 @@
+#ifndef PIT_C_INCLUDED
+#define PIT_C_INCLUDED
+
+
+#include <stdint.h>
+#include "mk20dx128.h"
+
+
+static void pit_start(unsigned int i, uint32_t n)
+{
+  /* i the timer identifier, in 0:3 */
+  /* n the period */
+
+  volatile uint32_t* const PIT_LDVAL = (&PIT_LDVAL0) + i * 4;
+  volatile uint32_t* const PIT_TCTRL = (&PIT_TCTRL0) + i * 4;
+  volatile uint32_t* const PIT_TFLG = (&PIT_TFLG0) + i * 4;
+  const uint32_t IRQ_PIT_CH = IRQ_PIT_CH0 + i;
+
+  /* enable pit clock */
+  SIM_SCGC6 |= 1 << 23;
+
+  /* section 37.3 */
+
+  /* enable the module clock */
+  PIT_MCR = 0;
+
+  /* timer load value */
+  /* countdown to 0 */
+  /* loaded after the timer expires */
+  *PIT_LDVAL = n;
+
+  /* clear pending interrupt */
+  *PIT_TFLG = 1 << 0;
+
+  /* enable interrupt vector */
+  NVIC_ENABLE_IRQ(IRQ_PIT_CH);
+
+  /* enable timer */
+  /* interrupt enabled */
+  *PIT_TCTRL = (1 << 1) | (1 << 0);
+}
+
+static void pit_stop(unsigned int i)
+{
+  volatile uint32_t* const PIT_TCTRL = (&PIT_TCTRL0) + i * 4;
+  const uint32_t IRQ_PIT_CH = IRQ_PIT_CH0 + i;
+  PIT_MCR = 1;
+  *PIT_TCTRL = 0;
+  NVIC_DISABLE_IRQ(IRQ_PIT_CH);
+}
+
+static inline void pit_clear_int(unsigned int i)
+{
+  volatile uint32_t* const PIT_TFLG = (&PIT_TFLG0) + i * 4;
+  *PIT_TFLG |= 1 << 0;
+}
+
+static inline unsigned int pit_is_int(unsigned int i)
+{
+  volatile uint32_t* const PIT_TFLG = (&PIT_TFLG0) + i * 4;
+  return *PIT_TFLG & (1 << 0);
+}
+
+static inline uint32_t pit_get_val(unsigned int i)
+{
+  volatile uint32_t* const PIT_CVAL = (&PIT_CVAL0) + i * 4;
+  return *PIT_CVAL;
+}
+
+
+#endif /* PIT_C_INCLUDED */
